50309ca1d43c0cdff3a670c66e8c09e026bb05f8
25LP5095 25LP5095 <25LP5095@admins-iMac.local>
2025-04-21 12:35:18 +0800

    s

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  env:{
+    TMDB_KEY : process.env.TMDB_KEY,
+    TMDB_TOKEN : process.env.TMDB_TOKEN,
+    TMDB_BASE_URL : process.env.TMDB_BASE_URL,
+  }
 };
 
 export default nextConfig;
