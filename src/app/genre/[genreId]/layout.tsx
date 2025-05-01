4472f6fbeda75fc166667dbdad56836e75940e8a
25LP5095 25LP5095 <25LP5095@admins-iMac.local>
2025-05-01 13:07:04 +0800

    aa

--- src/app/genre/[genreId]/layout.tsx
+++ src/app/genre/[genreId]/layout.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { ReactNode, Suspense } from "react";
 
+type GenreLayoutType = {
+  children: Readonly<ReactNode>;
+  genres: Readonly<React.ReactNode>;
+  movies: Readonly<React.ReactNode>;
+};
 const layout = () => {
-  return (
-    <div>layout</div>
-  )
-}
+  return <div>layout</div>;
+};
 
-export default layout
+export default layout;
